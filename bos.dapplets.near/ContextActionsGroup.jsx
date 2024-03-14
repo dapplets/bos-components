c36a056e0d6021eeb42b190295328ce8e6eebb65
Alexander Sakhaev <alsakhaev@gmail.com>
2024-03-14 14:43:29 +0300

    feat: layout manager knows about another widgets, so we have to filter them

--- bos.dapplets.near/ContextActionsGroup.jsx
+++ bos.dapplets.near/ContextActionsGroup.jsx
@@ -6,35 +6,31 @@ const {
   disableEditMode,
   widgets,
   apps,
-} = props
+} = props;
 
 State.init({
   show: false,
   showMenu: false,
 });
 
-const handleOnMouseEnter = () => State.update({ show: true })
+const handleOnMouseEnter = () => State.update({ show: true });
 
 const handleOnMouseLeave = () =>
-  state.showMenu
-    ? null
-    : isEditMode
-    ? null
-    : State.update({ show: false })
+  state.showMenu ? null : isEditMode ? null : State.update({ show: false });
 
-const handleOpenMenu = () => State.update({ showMenu: true, show: false })
+const handleOpenMenu = () => State.update({ showMenu: true, show: false });
 
-const handleCloseMenu = () => State.update({ showMenu: false, show: false })
+const handleCloseMenu = () => State.update({ showMenu: false, show: false });
 
 const handleSelectComponent = (app) => {
   State.update({ showMenu: false });
   createUserLink(app.id);
-}
+};
 
 const OverlayTriggerWrapper = styled.div`
   display: flex;
   z-index: 500;
-`
+`;
 
 const FloatingModal = styled.div`
   position: absolute;
@@ -52,23 +48,30 @@ const FloatingModal = styled.div`
       transform: translate(-50%, -50%);
     }
   }
-`
+`;
 
 return (
   <OverlayTriggerWrapper onMouseOver={handleOnMouseEnter}>
-    <Widget src='bos.dapplets.near/widget/LayoutManager.TriggerEar' props={{ show: state.show }}/>
-    {state.show ? <Widget
-      props={{
-        widgets,
-        isEditMode,
-        enableEditMode,
-        disableEditMode,
-        handleOpenMenu,
-        handleOnMouseLeave,
-        deleteUserLink,
-      }}
-      src="bos.dapplets.near/widget/LayoutManager.EarOpened"
-    /> : null}
+    <Widget
+      src="bos.dapplets.near/widget/LayoutManager.TriggerEar"
+      props={{ show: state.show }}
+    />
+    {state.show ? (
+      <Widget
+        props={{
+          widgets: widgets.filter(
+            (w) => w.isSuitable === undefined || w.isSuitable === true
+          ),
+          isEditMode,
+          enableEditMode,
+          disableEditMode,
+          handleOpenMenu,
+          handleOnMouseLeave,
+          deleteUserLink,
+        }}
+        src="bos.dapplets.near/widget/LayoutManager.EarOpened"
+      />
+    ) : null}
 
     {state.showMenu ? (
       <DappletOverlay>
@@ -85,4 +88,4 @@ return (
       </DappletOverlay>
     ) : null}
   </OverlayTriggerWrapper>
-)
+);
--- bos.dapplets.near/DefaultLayoutManager.jsx
+++ bos.dapplets.near/DefaultLayoutManager.jsx
@@ -6,8 +6,8 @@ const handleRemoveWidget = (linkId) => {
 
 const Container = styled.div`
   display: flex;
-    gap: 8px;
-    margin-left: 8px;
+  gap: 8px;
+  margin-left: 8px;
 `;
 
 const WidgetWrapper = styled.div`
@@ -17,53 +17,55 @@ const WidgetWrapper = styled.div`
 `;
 
 const WidgetBadgeWrapper = styled.div`
- position: absolute;
-    right: 0px;
-    top: 0px;
-    z-index: 1200;
-    background: rgba(255, 255, 255, 0.35);
-    width: 100%;
-    height: 100%;
-    display: flex;
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    border-radius: 4%;
-    backdrop-filter: blur(0.5px);
-    div {
-        padding: 2px;
-    }
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  z-index: 1200;
+  background: rgba(255, 255, 255, 0.35);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  border-radius: 4%;
+  backdrop-filter: blur(0.5px);
+  div {
+    padding: 2px;
+  }
 `;
 
 return (
   <Container>
-    {props.widgets.map((widget) => (
-      <WidgetWrapper key={widget.linkId}>
-        {props.isEditMode ? (
-          <WidgetBadgeWrapper
-            title={
-              widget.linkAuthorId === context.accountId
-                ? `Remove ${widget.src.split("widget/").pop()}`
-                : "disable in edit mode "
-            }
-            style={{
-              opacity: widget.linkAuthorId === context.accountId ? "1" : "0",
-            }}
-          >
-            {widget.linkAuthorId === context.accountId ? (
-              <Widget
-                src="bos.dapplets.near/widget/LayoutManager.DeleteWidgetButton"
-                props={{
-                  onClick: () => handleRemoveWidget(widget.linkId),
-                }}
-              />
-            ) : //   <Widget src="bos.dapplets.near/widget/LayoutManager.LockedWidgetBadge" />
-            null}
-          </WidgetBadgeWrapper>
-        ) : null}
-        <Widget src={widget.src} props={widget.props} />
-      </WidgetWrapper>
-    ))}
+    {props.widgets
+      .filter((w) => w.isSuitable === undefined || w.isSuitable === true)
+      .map((widget) => (
+        <WidgetWrapper key={widget.linkId}>
+          {props.isEditMode ? (
+            <WidgetBadgeWrapper
+              title={
+                widget.linkAuthorId === context.accountId
+                  ? `Remove ${widget.src.split("widget/").pop()}`
+                  : "disable in edit mode "
+              }
+              style={{
+                opacity: widget.linkAuthorId === context.accountId ? "1" : "0",
+              }}
+            >
+              {widget.linkAuthorId === context.accountId ? (
+                <Widget
+                  src="bos.dapplets.near/widget/LayoutManager.DeleteWidgetButton"
+                  props={{
+                    onClick: () => handleRemoveWidget(widget.linkId),
+                  }}
+                />
+              ) : //   <Widget src="bos.dapplets.near/widget/LayoutManager.LockedWidgetBadge" />
+              null}
+            </WidgetBadgeWrapper>
+          ) : null}
+          <Widget src={widget.src} props={widget.props} />
+        </WidgetWrapper>
+      ))}
   </Container>
 );
