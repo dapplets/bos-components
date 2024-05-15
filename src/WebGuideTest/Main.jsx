c9cebf8b7a0ae14c6f0c7bdf91198e0f50b12b2f
Alexander Sakhaev <alsakhaev@gmail.com>
2024-05-15 19:06:37 +0300

    fix: change twitter url

--- src/WebGuideTest/Main.jsx
+++ src/WebGuideTest/Main.jsx
@@ -1,24 +1,156 @@
-const OverlayTriggerWrapper = styled.div`
-  display: flex;
-  z-index: 500;
-
-  .OverlayTrigger {
-    position: absolute;
-    background: #db504a;
-    border: 1px solid #db504a;
-    width: 6px;
-    height: 49px;
-    right: -6px;
-    top: 10px;
-    border-radius: 0px 4px 4px 0px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    box-sizing: border-box;
-    z-index: 79;
-  }
-`
+const twitterConfig = {
+  chapters: [
+    {
+      id: "bos.dapplets.near/gateway/MutableWebExtension",
+      type: 'infobox',
+      if: { id: "1694995344461894022" }, // ToDo: should be another trigger
+      // showChecked: true,
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtension/Welcome",
+          title: "You’re entering Mutable Web",
+          status: [{ info: 'You must be logged in to make all features available' }],
+          content: "\nMutable Web is a new web3 paradigm that allows anyone to add custom functionality to existing websites and share them within your community— no matter who owns the website.\n\nBecome the co-owner of any website you are using!\n\n### Mutation switch\n\n![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/onboarding-video-001.webp)\n\nYou can switch between website mutations created by different communities.\n\n[See more examples on our playground](https://twitter.com/MrConCreator)\n",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebExtensionNotch",
+      type: 'callout',
+      placement: 'right',
+      strategy: 'fixed',
+      namespace: "mweb",
+      contextType: "notch",
+      injectTo: "hidden",
+      if: { id: { eq: "notch" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtensionNotch/Notch",
+          title: "Mutation switch",
+          status: [{ info: 'Click on the dropdown box' }],
+          content: "Mutation switch allows user to choose between applications’ sets.",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/1",
+      type: 'callout',
+      placement: 'right',
+      strategy: 'fixed',
+      namespace: "mweb",
+      contextType: "notch",
+      injectTo: "recently-used-mutations",
+      if: { id: { eq: "recently-used-mutations" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/1/1",
+          title: "The list of recently used mutations (1/2)",
+          status: [],
+          content: "The current mutation is highlighted with a “star” symbol. To add “favorite” status to a mutation on the current page, just click on the star. Now this mutation is active even if a new tab with the current site is open.",
+        },
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/1/2",
+          title: "The list of recently used mutations (2/2)",
+          status: [{ info: 'Click on the dropdown box below' }],
+          content: "The “trash” removes the mutation from the “recently used” list.",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/2",
+      type: 'callout',
+      strategy: 'fixed',
+      placement: 'right',
+      namespace: "mweb",
+      contextType: "notch",
+      injectTo: "hidden",
+      if: { id: { eq: "unused-mutations-title" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/2/1",
+          title: "All mutations",
+          status: [],
+          content: "This is the list of all available, previously unused mutations.",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/3",
+      type: 'callout',
+      strategy: 'fixed',
+      namespace: "mweb",
+      contextType: "notch",
+      injectTo: "hidden",
+      if: { id: { eq: "mutate-button" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebExtensionDropdown/3/1",
+          title: '"Mutate" button',
+          status: [],
+          content: 'The "Mutate" button allows users to create, fork, and edit mutations.',
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/app/Tipping/1",
+      type: 'callout',
+      namespace: "bos.dapplets.near/parser/twitter",
+      contextType: "post",
+      injectTo: "avatar",
+      if: { id: { eq: "1694995344461894022" } },
+      arrowTo: "insPoint",
+      pages: [
+        {
+          id: "bos.dapplets.near/app/Tipping/1/1",
+          status: [],
+          title: "Tipping app",
+          content: "If you want to modestly thank the author of the tweet you like...",
+        },
+      ],
+    },
+    {
+      id: "bos.dapplets.near/app/Tipping/2",
+      type: "callout",
+      namespace: "mweb",
+      contextType: "injected-widget",
+      injectTo: "hidden",
+      if: {
+        parentContextId: { eq: "1694995344461894022" },
+        widgetSrc: { eq: "bos.dapplets.near/widget/Tipping.Main" },
+      },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/app/Tipping/2/1",
+          status: [],
+          title: "Tipping app",
+          content: "... you can send them a tip through our application.",
+        },
+      ],
+    },
+    {
+      id: "bos.dapplets.near/mutation/EarTrigger",
+      type: 'callout',
+      namespace: "mweb",
+      contextType: "ear-trigger",
+      injectTo: "hidden",
+      if: { id: { eq: "1694995344461894022" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/mutation/EarTrigger/1",
+          status: [],
+          title: "Apps control panel",
+          content: 'We add the App control panel to every post. By using it the user can add to the right-on post or inside the panel widgets, which expands the browser workflow possibilities.',
+        },
+      ],
+    },
+  ],
+}
 
 const nearSocialConfig = {
   chapters: [
@@ -161,7 +293,7 @@ const nearSocialConfig = {
           id: "bos.dapplets.near/mutation/EarTrigger/copy/1",
           status: [],
           title: "Apps control panel",
-          content: '[See other Mutable Web facilities on X](https://augm.link/mutate/?t=https%3A%2F%2Ftwitter.com%2FMrConCreator&m=bos.dapplets.near%2FSandbox)',
+          content: '[See other Mutable Web facilities on X](https://augm.link/mutate/?t=https%3A%2F%2Ftwitter.com%2FMrConCreator&m=bos.dapplets.near%2Fmutation%2FSandbox)',
         },
       ],
     },
@@ -202,9 +334,32 @@ const nestedCalloutConfig = {
 
 // ToDo: move to a smart contract
 const guideConfigByLinkId = {
-  '3f969452807cb9f0ccc4809e679b861b': nearSocialConfig,
-  '11700d19b9540c6539acc24e5aaf3bb8': nestedCalloutConfig
+  // 'e4a93c289e9a2f042489a8c9fd827a31': twitterConfig,
+  '114a78557d9044ff8c6e0177bcbe690a': nearSocialConfig,
+  '0b492fd62c72d7ad87c7658e2b3a4f1e': nestedCalloutConfig
 }
+
+const OverlayTriggerWrapper = styled.div`
+  display: flex;
+  z-index: 500;
+
+  .OverlayTrigger {
+    position: absolute;
+    background: #db504a;
+    border: 1px solid #db504a;
+    width: 6px;
+    height: 49px;
+    right: -6px;
+    top: 10px;
+    border-radius: 0px 4px 4px 0px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    z-index: 79;
+  }
+`
 
 const guideConfig = guideConfigByLinkId[props.link.id]
 
