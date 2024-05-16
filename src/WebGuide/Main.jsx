b98343523254971dafde81f96493f13406f620c4
Alexander Sakhaev <alsakhaev@gmail.com>
2024-05-16 13:57:28 +0300

    feat: merge test webguide to prod

--- src/WebGuide/Main.jsx
+++ src/WebGuide/Main.jsx
@@ -1,26 +1,4 @@
-const OverlayTriggerWrapper = styled.div`
-  display: flex;
-  z-index: 500;
-
-  .OverlayTrigger {
-    position: absolute;
-    background: #db504a;
-    border: 1px solid #db504a;
-    width: 6px;
-    height: 49px;
-    right: -6px;
-    top: 10px;
-    border-radius: 0px 4px 4px 0px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    box-sizing: border-box;
-    z-index: 79;
-  }
-`
-
-const guideConfig = {
+const twitterConfig = {
   chapters: [
     {
       id: "bos.dapplets.near/gateway/MutableWebExtension",
@@ -32,7 +10,7 @@ const guideConfig = {
           id: "bos.dapplets.near/gateway/MutableWebExtension/Welcome",
           title: "You’re entering Mutable Web",
           status: [{ info: 'You must be logged in to make all features available' }],
-          content: "\nMutable Web is a new web3 paradigm that allows anyone to add custom functionality to existing websites and share them within your community— no matter who owns the website.\n\nBecome the co-owner of any website you are using!\n\n### Mutation switch\n\n![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/onboarding-video-001.webp)\n\nYou can switch between website mutations created by different communities.",
+          content: "\nMutable Web is a new web3 paradigm that allows anyone to add custom functionality to existing websites and share them within your community— no matter who owns the website.\n\nBecome the co-owner of any website you are using!\n\n### Mutation switch\n\n![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/onboarding-video-001.webp)\n\nYou can switch between website mutations created by different communities.\n\n[See more examples on our playground](https://twitter.com/MrConCreator)\n",
         },
       ]
     },
@@ -194,6 +172,203 @@ const guideConfig = {
   ],
 }
 
+const nearSocialConfig = {
+  chapters: [
+    {
+      id: "bos.dapplets.near/gateway/MutableWeb",
+      type: 'infobox',
+      if: { id: "1694995344461894022" }, // ToDo: should be another trigger
+      // showChecked: true,
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWeb/Welcome",
+          title: "You’re entering Mutable Web",
+          status: [{ info: 'You must be logged in to make all features available' }],
+          content: "Mutable Web is a new web3 paradigm that allows anyone to add custom functionality to existing websites and share them within your community — no matter who owns the website.\n\nBecome the co-owner of any website you are using!\n\n### Mutation switch\n\n![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/onboarding-008.png)\n\nYou can switch between website mutations created by different communities.\n\n[Mutable Web also runs on X](https://augm.link/mutate/?t=https%3A%2F%2Ftwitter.com%2FMrConCreator&m=bos.dapplets.near%2Fmutation%2FSandbox). Сheck its’ Web Guide there!",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebDropdown/1",
+      type: 'callout',
+      placement: 'right',
+      strategy: 'fixed',
+      namespace: "mweb",
+      contextType: "mweb-gateway",
+      injectTo: "mutation-wrapper",
+      if: { id: { eq: "mutation-wrapper" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebDropdown/1/1",
+          title: "Mutation switch",
+          status: [{ info: 'Click on the dropdown box' }],
+          content: "This is a mutation switcher for choosing between applications' sets.",
+        },
+      ]
+    },
+    {
+      id: "bos.dapplets.near/gateway/MutableWebDropdown/2",
+      type: 'callout',
+      strategy: 'fixed',
+      placement: 'right',
+      namespace: "mweb",
+      contextType: "mweb-gateway",
+      injectTo: "mutations-list",
+      if: { id: { eq: "mutations-list" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/gateway/MutableWebDropdown/2/1",
+          title: "All mutations",
+          status: [],
+          content: "Choose mutation (application set) from the list.",
+        },
+      ]
+    },
+    // {
+    //   id: "bos.dapplets.near/app/Tipping/1",
+    //   type: 'callout',
+    //   namespace: "bos.dapplets.near/parser/near-social-json",
+    //   contextType: "post",
+    //   injectTo: "avatar",
+    //   if: { id: { eq: "dappletsproject.near/119034910" } },
+    //   arrowTo: "insPoint",
+    //   pages: [
+    //     {
+    //       id: "bos.dapplets.near/app/Tipping/1/1",
+    //       status: [],
+    //       title: "Tipping app",
+    //       content: "If you want to modestly thank the author of the post you like...",
+    //     },
+    //   ],
+    // },
+    {
+      id: "bos.dapplets.near/app/Tipping/2",
+      type: "callout",
+      namespace: "mweb",
+      contextType: "injected-widget",
+      injectTo: "hidden",
+      if: {
+        parentContextId: { eq: "dappletsproject.near/119034910" },
+        widgetSrc: { eq: "bos.dapplets.near/widget/Tipping.Main" },
+      },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/app/Tipping/2/1",
+          status: [],
+          title: "Tipping app",
+          content: "If you want to modestly thank the author of the post you like, you can send them a tip through our application.",
+        },
+      ],
+    },
+    {
+      id: "bos.dapplets.near/mutation/EarTrigger",
+      type: 'callout',
+      namespace: "mweb",
+      contextType: "ear-trigger",
+      injectTo: "hidden",
+      if: { id: { eq: "dappletsproject.near/119034910" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/mutation/EarTrigger/1",
+          status: [],
+          title: "Apps control panel",
+          content: 'We’ve added a special container to all posts. By using it, the user can attach widgets, which will expand Near Social possibilities.',
+        },
+      ],
+    },
+    {
+      id: "bos.dapplets.near/mutation/EarTrigger/copy",
+      type: 'callout',
+      namespace: "mweb",
+      contextType: "ear-trigger",
+      injectTo: "hidden",
+      if: { id: { eq: "dappletsproject.near/119034910" } },
+      arrowTo: "context",
+      pages: [
+        {
+          id: "bos.dapplets.near/mutation/EarTrigger/copy/1",
+          status: [],
+          title: "Apps control panel",
+          content: '[See other Mutable Web facilities on X](https://augm.link/mutate/?t=https%3A%2F%2Ftwitter.com%2FMrConCreator&m=bos.dapplets.near%2Fmutation%2FSandbox)',
+        },
+      ],
+    },
+  ],
+}
+
+const nestedCalloutConfig = {
+  chapters: [{
+    id: "CalloutInCallout",
+    type: 'callout',
+    namespace: "mweb",
+    contextType: "wg-chapter",
+    injectTo: "hidden",
+    if: { id: { eq: "bos.dapplets.near/mutation/EarTrigger/copy" } }, // ID of the last chapter
+    arrowTo: "context",
+    pages: [
+      {
+        id: "CalloutInCallout/1",
+        status: [],
+        title: "Web Guides 1/3",
+        content: 'This Web Guide is also an app. Any user can create the same guide in order to explain other users some interface or a workflow. Web Guide consist of callouts, which point out elements (original or imbedded by other apps).',
+      },
+      {
+        id: "CalloutInCallout/2",
+        status: [],
+        title: "Web Guides 2/3",
+        content: '![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/webguide-004.png)\n\nWeb Guides can consist of several chapters.',
+      },
+      {
+        id: "CalloutInCallout/3",
+        status: [],
+        title: "Web Guides 3/3",
+        content: '![](https://raw.githubusercontent.com/dapplets/bos-components/main/assets/webguide-003.png)\n\nWeb Guide navigation is occuring via buttons.',
+      },
+    ],
+  }]
+}
+
+// ToDo: move to a smart contract
+const guideConfigByLinkId = {
+  // production (bos.dapplets.near/mutation/sandbox)
+  '9a6537b6efbb98ad4c12ef747c7e29e9': twitterConfig,
+  '8a1ca0a9eb03ce2725db7a553a9bcab7': nearSocialConfig,
+  "5c105a6a05975423d39a1e0d9447a22d": nestedCalloutConfig,
+  
+  // testing (bos.dapplets.near/mutation/testing2)
+  'b3777e928bd7ce9ad30eb636857a1853': twitterConfig,
+  '114a78557d9044ff8c6e0177bcbe690a': nearSocialConfig,
+  '0b492fd62c72d7ad87c7658e2b3a4f1e': nestedCalloutConfig,
+}
+
+const OverlayTriggerWrapper = styled.div`
+  display: flex;
+  z-index: 500;
+
+  .OverlayTrigger {
+    position: absolute;
+    background: #db504a;
+    border: 1px solid #db504a;
+    width: 6px;
+    height: 49px;
+    right: -6px;
+    top: 10px;
+    border-radius: 0px 4px 4px 0px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    z-index: 79;
+  }
+`
+
+const guideConfig = guideConfigByLinkId[props.link.id]
+
 const [showApp, setShowApp] = useState(true)
 const [chapterCounter, setChapterCounter] = useState(0)
 const [pageCounter, setPageCounter] = useState(0)
@@ -280,6 +455,7 @@ const ChapterWrapper = (props) => {
       src='bos.dapplets.near/widget/WebGuide.OverlayTrigger'
       loading={props?.children}
       props={{
+        id: currentChapter.id,
         type: currentChapter.type,
         placement: currentChapter.placement,
         strategy: currentChapter.strategy,
@@ -314,7 +490,6 @@ const ChapterWrapper = (props) => {
     />
   )
 }
-
 
 return showApp ? (guideConfig.chapters[chapterCounter]?.type === 'infobox' ? (
   <OverlayTriggerWrapper>
