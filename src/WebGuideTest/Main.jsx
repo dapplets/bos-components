4a4665335372ee3484eb10097c486ce36cc9b931
Nikita Teremovskiy <ni-2@mail.ru>
2024-07-01 11:38:58 +0300

    feat(webguide-test): context highlighting

--- src/WebGuideTest/Main.jsx
+++ src/WebGuideTest/Main.jsx
@@ -227,27 +227,6 @@ const twitterConfig = {
       skin: 'META_GUIDE',
     },
     {
-      id: "bos.dapplets.near/app/Paywall",
-      type: "callout",
-      namespace: "mweb",
-      contextType: "injected-widget",
-      injectTo: "hidden",
-      if: {
-        parentContextId: { eq: "1694995303642939408" },
-        widgetSrc: { eq: "bos.dapplets.near/widget/Paywall.Main" },
-      },
-      arrowTo: "context",
-      pages: [
-        {
-          id: "bos.dapplets.near/app/Paywall/1",
-          status: [],
-          title: "Paywall app",
-          content: "Paywall app allows users to add images to posts and sell access to them.",
-        },
-      ],
-      skin: 'META_GUIDE',
-    },
-    {
       id: "bos.dapplets.near/mutation/EarTrigger",
       type: 'callout',
       namespace: "mweb",
@@ -564,7 +543,6 @@ if (
 
 const handleClose = () => {
   setShowApp(false)
-  console.log('props', props)
 }
 
 const handleAction = () => {
@@ -613,7 +591,7 @@ const ChapterWrapper = (props) => {
   const currentPage = pages[pageCounter]
   if (!currentPage) return <></>
 
-  const status = currentPage.status.length&& Object.entries(currentPage.status[0])[0] // ToDo: mocked!!!
+  const status = currentPage.status.length && Object.entries(currentPage.status[0])[0] // ToDo: mocked!!!
 
   const buttons = []
   if (chapterCounter || pageCounter) {
@@ -696,7 +674,22 @@ const iconQuestionMark = (isActive) => (
       fill={isActive ? "#384BFF" : "white"}
     />
   </svg>
-);
+)
+
+const iconTimelineLatch = (color) => (
+  <svg
+    width="15"
+    height="19"
+    viewBox="0 0 15 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 18.6H0.39L7.27 8.42C7.36 8.24 7.31 8.15 7.12 8.15H4.28L7.27 2.7C7.36 2.52 7.29 2.43 7.07 2.43H3.26C3.15 2.43 3.06 2.49 2.97 2.61L0.19 10.01C0.17 10.19 0.23 10.28 0.38 10.28H3.13L0 18.6ZM8.5 12.27H8.77L13.99 4.6C14.04 4.52 14.05 4.45 14.03 4.4C14.01 4.35 13.95 4.33 13.86 4.33H11.76L13.94 0.3C14.06 0.1 14 0 13.76 0H11.02C10.89 0 10.79 0.0600001 10.72 0.19L8.64 5.67C8.61 5.76 8.61 5.83 8.65 5.88C8.69 5.93 8.75 5.95 8.84 5.95H10.88L8.5 12.27Z"
+      fill={color}
+    />
+  </svg>
+)
 
 return (
   <>
@@ -728,16 +721,34 @@ return (
         </DappletOverlay>
       </OverlayTriggerWrapper>
     ) : (
-      <DappletPortal
-        target={{
-          namespace: guideConfig.chapters[chapterCounter]?.namespace,
-          contextType: guideConfig.chapters[chapterCounter]?.contextType,
-          injectTo: guideConfig.chapters[chapterCounter]?.injectTo,
-          if: guideConfig.chapters[chapterCounter]?.if,
-          insteadOf: guideConfig.chapters[chapterCounter]?.insteadOf,
-        }}
-        component={ChapterWrapper}
-      />
+      <>
+        <DappletPortal
+          target={{
+            namespace: guideConfig.chapters[chapterCounter]?.namespace,
+            contextType: guideConfig.chapters[chapterCounter]?.contextType,
+            injectTo: guideConfig.chapters[chapterCounter]?.injectTo,
+            if: guideConfig.chapters[chapterCounter]?.if,
+            insteadOf: guideConfig.chapters[chapterCounter]?.insteadOf,
+          }}
+          component={ChapterWrapper}
+        />
+        <Highlighter
+          target={{ 
+            namespace: guideConfig.chapters[chapterCounter]?.namespace,
+            contextType: guideConfig.chapters[chapterCounter]?.contextType,
+            if: guideConfig.chapters[chapterCounter]?.if,
+          }}
+          styles={{
+            borderColor: '#14AE5C',
+            backgroundColor: 'rgb(56 255 63 / 10%)',
+            // borderStyle: 'dashed',
+          }}
+          // filled
+          // icon={iconTimelineLatch('#14AE5C')}
+          // icon={() => <></>}
+          // action={() => console.log('Highlighter action')}
+        />
+      </>
     )) : null}
   </>
 )
