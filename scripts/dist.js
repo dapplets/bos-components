c346039c7c4d303eb3e945cc022a085a585d4dcd
Alexander Sakhaev <alsakhaev@gmail.com>
2024-07-03 00:23:30 +0300

    build: merge two dist scripts

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
-const { getBosComponents } = require("./common/components");
+const { getBosComponents, getMWebComponents } = require("./common/components");
 
 const main = async () => {
   const bos = await getBosComponents();
   fs.writeFileSync("./build/bos.json", JSON.stringify(bos, null, 2));
+
+  const mweb = await getMWebComponents();
+  fs.writeFileSync("./build/mweb.json", JSON.stringify(mweb, null, 2));
 };
 
 main()
