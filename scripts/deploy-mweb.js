e9a9b26fcdee08270249e75a64183f5c9e3cc57b
Alexander Sakhaev <alsakhaev@gmail.com>
2024-07-02 23:38:43 +0300

    build: deploy mweb components one by one

--- scripts/deploy-mweb.js
+++ scripts/deploy-mweb.js
@@ -3,13 +3,44 @@ const { getMWebComponents } = require("./common/components");
 
 const [accountId, networkId] = process.argv.slice(2);
 
+const ProjectId = "dapplets.near";
+
 const main = async () => {
   const data = await getMWebComponents();
   const socialDb = connect(networkId, accountId);
 
-  await socialDb.set(data)
+  for (const accountId in data) {
+    const { app, mutation, parser } = data[accountId].settings[ProjectId];
 
-  console.log('Done!');
+    for (const appId in app) {
+      console.log(`Deploying ${accountId}/app/${appId}`);
+      await socialDb.set({
+        [accountId]: {
+          settings: { [ProjectId]: { app: { [appId]: app[appId] } } },
+        },
+      });
+    }
+
+    for (const mutationId in mutation) {
+      console.log(`Deploying ${accountId}/mutation/${mutationId}`);
+      await socialDb.set({
+        [accountId]: {
+          settings: { [ProjectId]: { mutation: { [mutationId]: mutation[mutationId] } } },
+        },
+      });
+    }
+
+    for (const parserId in parser) {
+      console.log(`Deploying ${accountId}/parser/${parserId}`);
+      await socialDb.set({
+        [accountId]: {
+          settings: { [ProjectId]: { parser: { [parserId]: parser[parserId] } } },
+        },
+      });
+    }
+  }
+
+  console.log("Done!");
 };
 
 main()
