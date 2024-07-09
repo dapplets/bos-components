fb2b95cc5af5f125f674bc8b9bcc7783a51ae08d
Lisofffa <lisofffa@gmail.com>
2024-07-09 04:37:42 +0300

    feat: added styles to edit web guide

--- src/widgets/WebGuideTest/OverlayTrigger.jsx
+++ src/widgets/WebGuideTest/OverlayTrigger.jsx
@@ -626,9 +626,16 @@ const themes = {
   },
 };
 
+const EditButtonsBlock = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  justify-content: space-evenly;
+`;
+
 const EditButton = styled.button`
   position: absolute;
-  left: 0;
+  right: 0;
   top: 0;
   margin: 0;
   padding: 0 10px;
@@ -636,22 +643,14 @@ const EditButton = styled.button`
   justify-content: center;
   align-items: center;
   border: none;
-  border-radius: 24px;
   background: none;
   cursor: pointer;
-  height: 24px;
-  background: white;
-  color: rgb(78, 119, 225);
+  outline: none;
+
   transition-duration: 0.15s;
 
   :hover {
-    color: #111;
-    background: #eee;
-  }
-
-  :active {
-    color: #000;
-    background: #ddd;
+    opacity: 0.5;
   }
 `;
 
@@ -706,17 +705,57 @@ const closeIcon = (
   </svg>
 );
 
+const editIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M6.76923 8H5.84615C5.35652 8 4.88695 8.1945 4.54073 8.54073C4.1945 8.88695 4 9.35652 4 9.84615V18.1538C4 18.6435 4.1945 19.1131 4.54073 19.4593C4.88695 19.8055 5.35652 20 5.84615 20H14.1538C14.6435 20 15.1131 19.8055 15.4593 19.4593C15.8055 19.1131 16 18.6435 16 18.1538V17.2308"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14.9999 6.00009L17.9999 9.00006M19.3849 7.58508C19.7787 7.19123 20 6.65707 20 6.10009C20 5.54311 19.7787 5.00894 19.3849 4.6151C18.9911 4.22126 18.4569 4 17.8999 4C17.3429 4 16.8088 4.22126 16.4149 4.6151L8 13V16H11L19.3849 7.58508Z"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const viewIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M5.77778 20C5.28889 20 4.87052 19.8261 4.52267 19.4782C4.17481 19.1304 4.00059 18.7117 4 18.2222V5.77778C4 5.28889 4.17422 4.87052 4.52267 4.52267C4.87111 4.17481 5.28948 4.00059 5.77778 4H18.2222C18.7111 4 19.1298 4.17422 19.4782 4.52267C19.8267 4.87111 20.0006 5.28948 20 5.77778V18.2222C20 18.7111 19.8261 19.1298 19.4782 19.4782C19.1304 19.8267 18.7117 20.0006 18.2222 20H5.77778ZM5.77778 18.2222H18.2222V7.55556H5.77778V18.2222ZM12 16.4444C10.7852 16.4444 9.70015 16.115 8.74489 15.456C7.78963 14.797 7.09689 13.9413 6.66667 12.8889C7.0963 11.837 7.78904 10.9816 8.74489 10.3227C9.70074 9.6637 10.7858 9.33393 12 9.33333C13.2142 9.33274 14.2996 9.66252 15.256 10.3227C16.2124 10.9828 16.9049 11.8382 17.3333 12.8889C16.9037 13.9407 16.2113 14.7964 15.256 15.456C14.3007 16.1156 13.2154 16.445 12 16.4444ZM12 14.2222C11.6296 14.2222 11.315 14.0927 11.056 13.8338C10.797 13.5748 10.6673 13.2599 10.6667 12.8889C10.6661 12.5179 10.7959 12.2033 11.056 11.9449C11.3161 11.6865 11.6308 11.5567 12 11.5556C12.3692 11.5544 12.6841 11.6841 12.9449 11.9449C13.2056 12.2056 13.3351 12.5203 13.3333 12.8889C13.3316 13.2575 13.2021 13.5724 12.9449 13.8338C12.6877 14.0951 12.3727 14.2246 12 14.2222ZM12 15.1111C12.6222 15.1111 13.1481 14.8963 13.5778 14.4667C14.0074 14.037 14.2222 13.5111 14.2222 12.8889C14.2222 12.2667 14.0074 11.7407 13.5778 11.3111C13.1481 10.8815 12.6222 10.6667 12 10.6667C11.3778 10.6667 10.8519 10.8815 10.4222 11.3111C9.99259 11.7407 9.77778 12.2667 9.77778 12.8889C9.77778 13.5111 9.99259 14.037 10.4222 14.4667C10.8519 14.8963 11.3778 15.1111 12 15.1111Z"
+      fill="white"
+    />
+  </svg>
+);
+
 const SuccessButton = styled.button`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 0px 20px;
-  width: 180px;
+  width: 155px;
   height: 42px;
-  background: #02193a;
-  border-radius: 50px;
-  border: none;
+  background: ${(props) => props.$background};
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.$border};
   cursor: pointer;
 
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
@@ -727,12 +766,94 @@ const SuccessButton = styled.button`
   font-size: 14px;
   line-height: 149%;
   text-align: center;
-  color: #ffffff;
+  color: ${(props) => props.$color};
   transition-duration: 0.2s;
 
   :hover {
-    background-color: rgb(32 45 63);
+    opacity: 0.5;
   }
+`;
+
+const EditInputsBlock = styled.div`
+  width: 100%;
+  padding: 10px;
+  border-radius: 10px;
+  background: #628bf5;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+`;
+
+const FloatingLabelContainer = styled.div`
+  background: #4e77e1;
+  border-radius: 10px;
+  overflow: hidden;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  position: relative;
+  flex: 0 1 auto;
+  display: flex;
+`;
+
+const FloatingLabelContainerArea = styled.div`
+  background: #4e77e1;
+  border-radius: 10px;
+  overflow: hidden;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  position: relative;
+  flex: 1 1 auto;
+  display: flex;
+`;
+
+const StyledInput = styled.input`
+  padding: 25px 10px 10px 10px;
+  background: inherit;
+  color: #fff;
+  line-height: 100%;
+  font-size: 14px;
+  border-radius: 10px;
+  border: 1px solid #e2e2e5;
+  width: 100%;
+  outline: none;
+
+  &:focus + label,
+  &:not(:placeholder-shown) + label {
+    top: 0.5rem;
+    font-size: 12px;
+    color: #bbccd0;
+    left: 10px;
+  }
+`;
+
+const StyledTextarea = styled.textarea`
+  padding: 25px 10px 10px;
+  background: inherit;
+  color: rgb(255, 255, 255);
+  line-height: 100%;
+  font-size: 14px;
+  border-radius: 10px;
+  border: 1px solid rgb(226, 226, 229);
+  width: 100%;
+  outline: none;
+
+  &:focus + label,
+  &:not(:placeholder-shown) + label {
+    top: 0.5rem;
+    font-size: 12px;
+    color: #bbccd0;
+    left: 10px;
+  }
+`;
+
+const StyledLabel = styled.label`
+  position: absolute;
+  top: 25px;
+  left: 10px;
+  font-size: 1rem;
+  color: #bbccd0;
+  pointer-events: none;
+  transition: all 0.2s ease 0s;
 `;
 
 const {
@@ -748,19 +869,45 @@ const {
   onDoNotShowChange,
   skin,
   // todo: new props
-
+  saveTitle,
   saveData,
-
 } = props;
 
 // todo: new
 const [newData, setNewData] = useState(content ?? "");
+const [newTitle, setNewTitle] = useState(title ?? "");
 const [isEditMode, setEditMode] = useState(false);
 // todo: new
+
+useEffect(() => {
+  try {
+    const storedData = localStorage.setItem(`${props.id}newData`, newData);
+    const storedTitle = localStorage.setItem(`${props.id}newTitle`, newTitle);
+
+    if (storedData) {
+      setNewData(storedData);
+    }
+    if (storedTitle) {
+      setNewTitle(storedTitle);
+    }
+  } catch (error) {
+    console.error("Error accessing localStorage", error);
+  }
+}, []);
+
+// todo: new
 const handleSave = () => {
-  saveData(newData);
-  setNewData("");
-  // setShow(false);
+  // todo: uncomment when will be contract function
+  // saveData(newData);
+  // saveTitle(newTitle);
+
+  try {
+    localStorage.setItem(`${props.id}newData`, newData);
+    localStorage.setItem(`${props.id}newTitle`, newTitle);
+    setEditMode(false);
+  } catch (error) {
+    console.error("Error accessing localStorage", error);
+  }
 };
 
 const header = (
@@ -769,7 +916,16 @@ const header = (
     {/* {context.accountId === props.link?.authorId ? 
        ( */}
     <TopLine>
-      <EditButton onClick={() => setEditMode(true)}>Edit data</EditButton>
+      {navi ? (
+        <>
+          <CalloutHeaderCaption $col={themes[skin].colorMain}>
+            Step {navi?.currentChapterIndex + 1}
+          </CalloutHeaderCaption>
+        </>
+      ) : null}
+      <EditButton onClick={() => setEditMode(!isEditMode)}>
+        {isEditMode ? viewIcon : editIcon}
+      </EditButton>
     </TopLine>
     {/* ) 
           : (
@@ -877,33 +1033,60 @@ const callout = (
 
     {!content || isEditMode ? (
       <>
-        {" "}
-        <Header>
-           <Title $type={props.type} $col={themes[skin].colorMain}>Add data</Title>
-          <CloseButton onClick={() => handleClose(false, viewedPages)}>
-            {closeIcon}
-          </CloseButton>
-        </Header>
-        <textarea
-          autoFocus
-          style={{ width: "100%", height: "100%" }}
-          onChange={(e) => setNewData(e.target.value)}
-          value={newData}
-        />
-        <div
-          style={{
-            display: "flex",
-            width: "80%",
-            justifyContent: "space-evenly",
-          }}
-        >
+        <EditInputsBlock>
+          <FloatingLabelContainer>
+            <StyledInput id={"target"} type={"text"} value={props.type} />
+            <StyledLabel htmlFor={"target"}>Target</StyledLabel>
+          </FloatingLabelContainer>
+          {title ? (
+            <FloatingLabelContainer>
+              <StyledInput
+                id={"title"}
+                type={"text"}
+                defaultValue={newTitle}
+                onChange={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setNewTitle(e.target.value);
+                }}
+              />
+              <StyledLabel htmlFor={"title"}>Page name</StyledLabel>
+            </FloatingLabelContainer>
+          ) : null}
+
+          <FloatingLabelContainerArea>
+            <StyledTextarea
+              id={"description"}
+              defaultValue={newData}
+              onChange={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setNewData(e.target.value);
+              }}
+            ></StyledTextarea>
+            <StyledLabel htmlFor={"description"}>Description</StyledLabel>
+          </FloatingLabelContainerArea>
+        </EditInputsBlock>
+        <EditButtonsBlock>
           {isEditMode ? (
-            <SuccessButton onClick={() => setEditMode(false)}>
+            <SuccessButton
+              $background={"#4E77E1"}
+              $border={"#fff"}
+              $color={"#fff"}
+              onClick={() => setEditMode(false)}
+            >
               Cancel
             </SuccessButton>
           ) : null}
-          <SuccessButton onClick={handleSave}>Save</SuccessButton>
-        </div>
+          <SuccessButton
+            $background={"#fff"}
+            $border={"#fff"}
+            $color={"#4E77E1"}
+            onClick={handleSave}
+          >
+            Save guide
+          </SuccessButton>
+        </EditButtonsBlock>
       </>
     ) : (
       <>
@@ -942,33 +1125,60 @@ const infobox = (
 
     {!content || isEditMode ? (
       <>
-        {" "}
-        <Header>
-          <h1>Add data</h1>
-          <CloseButton onClick={() => handleClose(false, viewedPages)}>
-            {closeIcon}
-          </CloseButton>
-        </Header>
-        <textarea
-          autoFocus
-          style={{ width: "100%", height: "100%" }}
-          onChange={(e) => setNewData(e.target.value)}
-          value={newData}
-        />
-        <div
-          style={{
-            display: "flex",
-            width: "80%",
-            justifyContent: "space-evenly",
-          }}
-        >
+        <EditInputsBlock>
+          <FloatingLabelContainer>
+            <StyledInput id={"target"} type={"text"} value={props.type} />
+            <StyledLabel htmlFor={"target"}>Target</StyledLabel>
+          </FloatingLabelContainer>
+          {title ? (
+            <FloatingLabelContainer>
+              <StyledInput
+                id={"title"}
+                type={"text"}
+                defaultValue={newTitle}
+                onChange={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setNewTitle(e.target.value);
+                }}
+              />
+              <StyledLabel htmlFor={"title"}>Page name</StyledLabel>
+            </FloatingLabelContainer>
+          ) : null}
+
+          <FloatingLabelContainerArea>
+            <StyledTextarea
+              id={"description"}
+              defaultValue={newData}
+              onChange={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setNewData(e.target.value);
+              }}
+            ></StyledTextarea>
+            <StyledLabel htmlFor={"description"}>Description</StyledLabel>
+          </FloatingLabelContainerArea>
+        </EditInputsBlock>
+        <EditButtonsBlock>
           {isEditMode ? (
-            <SuccessButton onClick={() => setEditMode(false)}>
+            <SuccessButton
+              $background={"#4E77E1"}
+              $border={"#fff"}
+              $color={"#fff"}
+              onClick={() => setEditMode(false)}
+            >
               Cancel
             </SuccessButton>
           ) : null}
-          <SuccessButton onClick={handleSave}>Save</SuccessButton>
-        </div>
+          <SuccessButton
+            $background={"#fff"}
+            $border={"#fff"}
+            $color={"#4E77E1"}
+            onClick={handleSave}
+          >
+            Save guide
+          </SuccessButton>
+        </EditButtonsBlock>
       </>
     ) : (
       <>
