4b54579fbc6b3d43f8ce0e88f203578e442a8552
Alexander Sakhaev <alsakhaev@gmail.com>
2024-08-19 13:33:45 +0300

    refactor: move augm.link filter from the engine to the app manifest level

--- src/widgets/BlinkExample/Main.jsx
+++ src/widgets/BlinkExample/Main.jsx
@@ -2,4 +2,8 @@ const handleClick = () => {
   console.log(props.context)
 }
 
-return <button onClick={handleClick}>Blink</button>
+return (
+  <button type="button" className="btn btn-primary btn-sm" onClick={handleClick}>
+    Blink
+  </button>
+)
