36139920536f742e7b88d476b8df8c673c67302f
Alexander Sakhaev <alsakhaev@gmail.com>
2024-08-23 15:00:48 +0300

    feat: insert components into first visible context only (DAP-4727) (#60)

--- src/widgets/WebGuide/Main.jsx
+++ src/widgets/WebGuide/Main.jsx
@@ -118,6 +118,13 @@ const deepCopy = (obj) => JSON.parse(JSON.stringify(obj))
 // ToDo: naive deep compare
 const isDeepEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 
+const clearTreeBranch = (node) => ({
+  namespace: node.namespace,
+  type: node.type,
+  id: node.id,
+  parent: node.parent ? clearTreeBranch(node.parent) : undefined,
+})
+
 const configTemplate = {
   action: true,
 }
@@ -387,7 +394,7 @@ const handleTargetSet = (newTarget) => {
   const updatedChapter = updatedConfig.chapters[chapterCounter]
 
   updatedChapter.type = 'callout'
-  updatedChapter.target = newTarget
+  updatedChapter.target = newTarget ? clearTreeBranch(newTarget) : null
 
   setEditingConfig(updatedConfig)
   saveConfigToLocalStorage(updatedConfig)
@@ -738,9 +745,15 @@ return (
         </OverlayTriggerWrapper>
       ) : currentChapter.target ? (
         <>
-          <DappletPortal inMemory target={currentChapter.target} component={ChapterWrapper} />
+          <DappletPortal
+            inMemory
+            target={currentChapter.target}
+            component={ChapterWrapper}
+            isFirstTargetOnly={currentChapter.isFirstTargetOnly}
+          />
           <Highlighter
             target={currentChapter.target}
+            isFirstTargetOnly={currentChapter.isFirstTargetOnly}
             styles={{
               borderColor: '#14AE5C',
               backgroundColor: 'rgb(56 255 63 / 10%)',
