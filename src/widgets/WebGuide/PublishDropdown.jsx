73bd396e5b3d7e0ee840731ef45bc4dce367f2e6
Alexander Sakhaev <alsakhaev@gmail.com>
2024-09-09 15:32:23 +0300

    fix: dropdown color

--- src/widgets/WebGuide/PublishDropdown.jsx
+++ src/widgets/WebGuide/PublishDropdown.jsx
@@ -93,7 +93,7 @@ const arrowIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8" fill="none">
     <path
       d="M1 1L7 7L13 1"
-      stroke="#4E77E1"
+      stroke="var(--bgMain)"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
